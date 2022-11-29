e1be1e305a018ed3bb40d29d558bec0f7b3695dd
Dhruv Mohindru <dmohindru@gmail.com>
2022-11-30 06:54:49 +1100

    Completed section 4

---
+++ code_along/section4/springsecsection4/src/main/java/dev/dmohindru/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package dev.dmohindru.repository;
+
+import dev.dmohindru.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+    List<Customer> findByEmail(String email);
+}
