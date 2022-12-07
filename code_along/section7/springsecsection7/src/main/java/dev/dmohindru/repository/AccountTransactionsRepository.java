834e4069d24753e414a4c295f0b9625fb38d90de
Dhruv Mohindru <dmohindru@gmail.com>
2022-12-08 07:05:21 +1100

    Started new section 7

---
+++ code_along/section7/springsecsection7/src/main/java/dev/dmohindru/repository/AccountTransactionsRepository.java
@@ -0,0 +1,15 @@
+package dev.dmohindru.repository;
+
+
+import dev.dmohindru.model.AccountTransactions;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountTransactionsRepository extends CrudRepository<AccountTransactions, Long> {
+	
+	List<AccountTransactions> findByCustomerIdOrderByTransactionDtDesc(int customerId);
+
+}
